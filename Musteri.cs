5e1d55e8d0523ab4c68813dc4123bf59f92b6f06
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 02:00:13 +0300

    Proje dosyası ekle.

---
+++ Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProperty
+{
+    internal class Musteri
+
+    {
+        private int kilo;
+        public int KİLO { get { return kilo; } set { kilo = value; } }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using MyProperty;
+
+Musteri musteri1 = new Musteri();
+musteri1.KİLO = 85;
+
+Console.WriteLine("Müşteri Kilo :{0}", (musteri1.KİLO));
